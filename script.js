c677fbd80b0bb8bb36d70ac4802ee266f774b505
root <root@LAPTOP-6RNK2QH5.localdomain>
2025-07-23 17:02:01 +0800

    added resize and reset button

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const container = document.querySelector(".container");
 function makeGrid(size){
     container.innerHTML = ""
 
+    squareSize = 960/size;
+
     totalSquares = size * size;
 
     let isMouseClicked = false;
@@ -22,6 +24,8 @@ function makeGrid(size){
     for (let i = 0; i < totalSquares; i++ ){
         const square = document.createElement("div");
         square.classList.add("square");
+        square.style.width = squareSize + "px";
+        square.style.height = squareSize + "px";
         square.addEventListener("mouseenter", function () {
             if(isMouseClicked){
                 square.style.backgroundColor = "black";            }
@@ -30,4 +34,23 @@ function makeGrid(size){
     }
 }
 
-makeGrid(16)
+const resize = document.querySelector("#resizeBtn");
+
+resize.addEventListener("click",function (){
+
+    const userInput = parseInt(prompt("Grid size(MAX 100): "));
+
+    while (userInput > 100){
+        userInput = parseInt(prompt("Grid size(MAX 100): "));
+    }
+
+    makeGrid(userInput);
+});
+
+const reset = document.querySelector("#resetBtn");
+
+reset.addEventListener("click", function () {
+    makeGrid(16);
+});
+
+makeGrid(16);
