15caf6652013476723fdac9024ab8347a68d7a32
root <root@LAPTOP-6RNK2QH5.localdomain>
2025-07-23 15:09:43 +0800

    added grid

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-const container = document.querySelector("container");
+const container = document.querySelector(".container");
 
+function makeGrid(size){
+    container.innerHTML = ""
+
+    totalSquares = size * size;
+
+    for (let i = 0; i < totalSquares; i++ ){
+        const square = document.createElement("div");
+        square.classList.add("square")
+        container.appendChild(square);
+    }
+}
+
+makeGrid(16)
