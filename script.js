5b42ad6224d20264a4e215f8c27241d39460ac96
root <root@LAPTOP-6RNK2QH5.localdomain>
2025-07-23 20:08:42 +0800

    final

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const container = document.querySelector(".container");
 function makeGrid(size){  //make grid
     container.innerHTML = ""
 
-    squareSize = 960/size;
+    squareHeight = 650/size;
+    squareWidth = 800/size;
 
     totalSquares = size * size;
 
@@ -24,8 +25,8 @@ function makeGrid(size){  //make grid
     for (let i = 0; i < totalSquares; i++ ){
         const square = document.createElement("div");
         square.classList.add("square");
-        square.style.width = squareSize + "px";
-        square.style.height = squareSize + "px";
+        square.style.width = squareWidth + "px";
+        square.style.height = squareHeight + "px";
         square.addEventListener("mouseenter", function () {
             if(isMouseClicked){
                 if(isColor) {
@@ -43,26 +44,7 @@ function makeGrid(size){  //make grid
     }
 }
 
- makeGrid(16);
-
-const resize = document.querySelector("#resizeBtn"); //resize
-
-resize.addEventListener("click",function (){
-
-    const userInput = parseInt(prompt("Grid size(MAX 100): "));
-
-    while (userInput > 100){
-        userInput = parseInt(prompt("Grid size(MAX 100): "));
-    }  
-
-    makeGrid(userInput);
-});
-
-const reset = document.querySelector("#resetBtn"); //reset
-
-reset.addEventListener("click", function () {
-    makeGrid(16);
-});
+makeGrid(16);
 
 const color = document.querySelector("#colorBtn"); //color
 
@@ -72,4 +54,21 @@ color.addEventListener("click", function() {
     isColor = !isColor;
 })
 
+const rightKnob = document.querySelector(".right-knob");
+const rightKnobLabel = document.querySelector(".rKnob-label");
 
+const sizes = [16, 32, 64, 100];
+let currentSizeIndex = 0;
+
+rightKnob.addEventListener("click", function () {
+    currentSizeIndex = (currentSizeIndex + 1) % sizes.length;
+    const newSize = sizes[currentSizeIndex];
+    rightKnobLabel.textContent = newSize;
+    makeGrid(newSize);
+});
+
+const leftKnob = document.querySelector(".left-knob");
+
+leftKnob.addEventListener("click", function () {
+    container.innerHTML = ""
+});
